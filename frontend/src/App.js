eb8505e706d9e475488b92a61afded02094770f3
YazeedWidyan <yazeedwidyany@gmail.com>
2022-09-27 00:31:33 +0300

    complete context setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import Router from "./router";
+import { createContext, useState } from "react";
+export const appContext = createContext();
 
 function App() {
-  return <Router />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <appContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      <Router />
+    </appContext.Provider>
+  );
 }
 
 export default App;
