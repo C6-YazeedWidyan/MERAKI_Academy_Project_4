3b48d2d2c00a3bd00c34f2c4a578c07e4472cbc1
YazeedWidyan <yazeedwidyany@gmail.com>
2022-09-29 17:48:11 +0300

    complete update from admin game

--- frontend/src/containers/Dashboard/index.js
+++ frontend/src/containers/Dashboard/index.js
@@ -4,6 +4,7 @@ import "./style.css";
 
 import axios from "axios";
 import { AuthContext } from "../../contexts/AuthContext";
+import Select from "react-select";
 
 const Dashboard = () => {
   const { token, logout } = useContext(AuthContext);
@@ -11,7 +12,30 @@ const Dashboard = () => {
   const [games, setGames] = useState("");
   const [show, setShow] = useState(false);
   const [message, setMessage] = useState("");
+  const [gameId, setGameId] = useState("");
   const [userId, setUserId] = useState("");
+  console.log(gameId);
+  const [isEdit, setIsEdit] = useState(false);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [poster, setPoster] = useState("");
+  const [logo, setLogo] = useState("");
+  const [cover, setCover] = useState("");
+  const [ads, setAds] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [description, setDescription] = useState("");
+  const [platform, setPlatform] = useState("PC");
+  const [inStock, setinStock] = useState(false);
+  const [releaseDate, setReleaseDate] = useState("");
+
+  const options = [
+    { label: "action", value: "Action" },
+    { label: "sport", value: "Sport" },
+  ];
+
+  const onChange = (opt) => {
+    setCategories([...opt]);
+  };
 
   const getAllGames = async () => {
     try {
@@ -32,6 +56,47 @@ const Dashboard = () => {
       }
       setMessage("Error happened while Get Data, please try again");
     }
+  };
+
+  const showEdit = (key) => {
+    setIsEdit(true);
+    setGameId(key);
+  };
+
+  const updateGame = (id) => {
+    const category = categories.map((item) => item.value);
+    console.log(id);
+    axios
+      .put(
+        `http://localhost:5000/games/${id}`,
+        {
+          name,
+          price,
+          poster,
+          logo,
+          cover,
+          ads,
+          category,
+          description,
+          platform,
+          inStock,
+          releaseDate,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        getAllGames();
+        console.log(res);
+        setIsEdit(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsEdit(false);
+      });
   };
 
   const deleteGame = (id) => {
@@ -66,7 +131,7 @@ const Dashboard = () => {
       <br />
       {show &&
         games.map((game, index) => (
-          <div key={index} className="game">
+          <div key={game._id} className="game">
             <div>{game.name}</div>
             <button
               className="delete-btn"
@@ -76,6 +141,133 @@ const Dashboard = () => {
             >
               Delete Game
             </button>
+            <div className="update">
+              {isEdit && gameId == game._id ? (
+                <>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
+                    />
+                    <br />
+                    <input
+                      type="number"
+                      placeholder="Price"
+                      onChange={(e) => {
+                        setPrice(e.target.value);
+                      }}
+                    />
+                    <br />
+                    <input
+                      type="text"
+                      placeholder="Poster"
+                      onChange={(e) => {
+                        setPoster(e.target.value);
+                      }}
+                    />
+                    <br />
+                    <input
+                      type="text"
+                      placeholder="Logo"
+                      onChange={(e) => {
+                        setLogo(e.target.value);
+                      }}
+                    />
+                    <br />
+                    <input
+                      type="text"
+                      placeholder="Cover"
+                      onChange={(e) => {
+                        setCover(e.target.value);
+                      }}
+                    />
+                    <br />
+                    <input
+                      type="text"
+                      placeholder="Description"
+                      onChange={(e) => {
+                        setDescription(e.target.value);
+                      }}
+                    />
+                    <br />
+                    <select
+                      onChange={(e) => {
+                        setPlatform(e.target.value);
+                      }}
+                    >
+                      <option value="PC">PC</option>
+                      <option value="Nintendo Switch">Nintendo Switch</option>
+                      <option value="PlayStation 4">PlayStation 4</option>
+                      <option value="PlayStation 5">PlayStation 5</option>
+                      <option value="Xbox One">Xbox One</option>
+                      <option value="Xbox Series X/S">Xbox Series X/S</option>
+                    </select>
+                    <br />
+                    <div>
+                      <Select
+                        isMulti
+                        onChange={onChange}
+                        options={options}
+                        value={categories}
+                      />
+                    </div>
+                    <br />
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={inStock}
+                        onChange={() => {
+                          setinStock(!inStock);
+                        }}
+                      />
+                      in Stock
+                    </label>
+                    <br />
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={ads}
+                        onChange={() => {
+                          setAds(!ads);
+                        }}
+                      />
+                      Ads?
+                    </label>
+                    <br />
+                    <input
+                      type="text"
+                      placeholder="Release Date"
+                      onChange={(e) => {
+                        setReleaseDate(e.target.value);
+                      }}
+                    />
+                  </div>
+                </>
+              ) : null}
+              {!isEdit ? (
+                <button
+                  className="update-btn-1"
+                  onClick={() => {
+                    showEdit(game._id);
+                  }}
+                >
+                  Update
+                </button>
+              ) : null}
+              {isEdit && gameId == game._id ? (
+                <button
+                  className="update-btn-2"
+                  onClick={() => {
+                    updateGame(game._id);
+                  }}
+                >
+                  Update
+                </button>
+              ) : null}
+            </div>
           </div>
         ))}
       {message && <div>{message}</div>}
